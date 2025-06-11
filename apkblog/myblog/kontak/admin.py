89aa20b287bab5fa0979d04d6218e522c62b7e0b
Rafa <rafacrimson2@gmail.com>
2025-06-11 09:18:11 +0800

    Stop tracking apkblog folder

--- apkblog/myblog/kontak/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import KontakModel
-
-# Register your models here.
-
-admin.site.register(KontakModel)
--- apkblog/myblog/myblog/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-
-def index(req):
-    context = {
-        'judul':'home',
-        'heading':'selamat datang di halaman home',
-    }
-    return render(req, 'index.html', context)
